3540de6462d7e32e64fd58e3d7cfe182ff5936eb
dEaDsHoT1235 <114793901+dEaDsHoT1235@users.noreply.github.com>
2022-11-29 17:10:22 +0530

    class5-Operators and Expressions-Cipherschools

---
+++ class5.py
@@ -0,0 +1,36 @@
+# arthametic operators
+print((5+5)) #sum 
+print((5-2)) #subtraction
+print(10/3) #divide
+print(10//3) #roundoff divide
+print(10%3) 
+print(2**3) #exponetial power
+print("laksh"+"ay") #artametic operations can be used in any thing
+print("lakshay" " " "singh") #" " means a space in between
+print("abc"*6) #it multiples the str and gives a sol like abcabcabcabcabcabc
+#python in 5 times slower  than c++ and java
+
+# comparision operators
+# ans in true or false
+
+
+print(1 == 1) #equvalent 
+print(1 == 2) #equvalent
+print(2>3,2<3) #greaterthan and lessthan
+print("ab" < "z") #true because of lexicographic order (the char which come first will be lesses in logic of coding)
+print("a"<"A") # false
+print(True and False)#boolean
+print(True or False)
+print(True and 1)
+print(1 and 0)
+print(1 and 5)
+print("lakshay" and 6)
+print("" or 1.4)#
+print("")
+# '' is falsy 
+# "lakshay" is truthy
+# 0 is falsey
+# 1234 is truthy 
+print("" and 0)
+print("lakshay" and 6)#for and if first one is true then b is ans
+# and viseversa
