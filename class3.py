e4b85b8357c48f6d913cb638f7dff80b95b9c0d3
dEaDsHoT1235 <114793901+dEaDsHoT1235@users.noreply.github.com>
2022-11-29 17:07:48 +0530

    class3-Print Command Functionalities-cipherschools

---
+++ class3.py
@@ -0,0 +1,15 @@
+# print(id(123))
+# gives you a unique no. id 
+#  identifer
+# rules
+# 1 first letter must be a alphabeta A a _ 
+# no digits(0-9) for first character
+# identifers are case sensitive 
+print("lakshay singh", end=";")
+print(1, end=",")
+print(1,"lakshay",1+5j,1.2, sep=",")
+# print can except infinte no. of elements
+# 2 diff print means a new for the second print
+print("a")
+print("b")
+print("c")
